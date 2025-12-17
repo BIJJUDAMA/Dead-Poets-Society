32aa43871b700ee1a48518004e8345db597ff659
Nitansh Shankar <167768158+BIJJUDAMA@users.noreply.github.com>
2025-12-18 01:36:53 +0530

    fix: fetching poets properly

--- src/app/poets/page.js
+++ src/app/poets/page.js
@@ -1,5 +1,13 @@
 import UsersPage from '@/views/UsersPage';
+import { supabase } from '@/supabase/config';
 
-export default function Page() {
-    return <UsersPage />;
+export const revalidate = 3600;
+
+export default async function Page() {
+    const { data: users, error } = await supabase
+        .from('profiles')
+        .select('*')
+        .not('display_name', 'is', null);
+
+    return <UsersPage initialUsers={users || []} />;
 }
