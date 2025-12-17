ae38ac3fe2a837ec2722a25fb2d28e517c1479fd
Nitansh Shankar <167768158+BIJJUDAMA@users.noreply.github.com>
2025-12-18 00:17:58 +0530

    chore: lazy load login with ssr disabled

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,4 +1,6 @@
-import LoginPage from '@/views/LoginPage';
+import dynamic from 'next/dynamic';
+
+const LoginPage = dynamic(() => import('@/views/LoginPage'), { ssr: false });
 
 export default function Page() {
     return <LoginPage />;
